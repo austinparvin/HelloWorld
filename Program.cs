06b2aa3a9227c3cc4f8c2e0ce07931d78a33acac
Austin Parvin <austin.parvin@gmail.com>
2020-02-10 15:08:30 -0500

    Init comment

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //WriteLine is written Pascal cased: camel-case with first word capitalized
+      Console.WriteLine("Hello, World, my is Austin Parvin");
+    }
+  }
+}
